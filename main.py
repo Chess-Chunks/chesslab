45e96ad099a92e481b62532960a55dad1138b970
Joel Hutchinson <hutchy100@live.co.uk>
2025-06-20 18:50:38 -0300

    feat(api): add CORS middleware to FastAPI

--- main.py
+++ main.py
@@ -1,9 +1,18 @@
 """FastAPI application with basic routes"""
 
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from api.endpoints import chesscom_stats, lichess_stats
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:5173"],
+    allow_credentials=True,
+    allow_methods=["*"], 
+    allow_headers=["*"], 
+)
 
 app.include_router(chesscom_stats.router)
 app.include_router(lichess_stats.router)
