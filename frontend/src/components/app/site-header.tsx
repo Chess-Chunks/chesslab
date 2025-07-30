dc0127d6716e9f375f9c97387fed92620018b888
Joel Hutchinson <hutchy100@live.co.uk>
2025-07-30 18:37:19 -0300

    feat: update site header with new logo images and adjust title size

--- frontend/src/components/app/site-header.tsx
+++ frontend/src/components/app/site-header.tsx
@@ -2,9 +2,19 @@ import { ModeToggle } from "../ui/mode-toggle";
 
 export function SiteHeader() {
   return (
-    <header className="flex h-12 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-data-[collapsible=icon]/sidebar-wrapper:h-12">
+    <header className="flex h-14 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-data-[collapsible=icon]/sidebar-wrapper:h-12">
       <div className="flex w-full items-center px-4 ">
-        <h1 className="text-base font-medium">Chess Lab</h1>
+        <img
+          src="/chesslab-logo-light.png"
+          alt="Chess Lab"
+          className="size-12 dark:hidden"
+        />
+        <img
+          src="/chesslab-logo-dark.png"
+          alt="Chess Lab"
+          className="size-12 hidden dark:block"
+        />
+        <h1 className="text-3xl font-medium">Chess Lab</h1>
         <div className="ml-auto flex items-center gap-2">
           <ModeToggle />
         </div>
